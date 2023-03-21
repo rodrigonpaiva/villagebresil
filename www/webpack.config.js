8fe97b1dd9562255891c866d45a8225ea8aecf37
Rodrigo Paiva <rodrigonpaiva@hotmail.com>
2023-03-21 14:51:18 +0100

    Adjust

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -4,14 +4,24 @@ module.exports = {
     entry: './src/index.js',
     output:{
         filename: 'script.js',
-        path:path.resolve(__dirname, 'dist')
+        path:path.resolve(__dirname, 'dist/assets/js')
     },
     mode: 'production',
     module:{
         rules:[
         {test:/\.css$/, use:['style-loader','css-loader']},
-        {test:/\.(png|jpg|gif|svg)$/, use:['file-loader']},
-        {test:/\.scss$/, use:['style-loader','css-loader','sass-loader']}
+        {test:/\.scss$/, use:['style-loader','css-loader','sass-loader']},
+        {
+            test:/\.(png|jpg|gif|svg)$/, 
+            use:[
+                {
+                    loader:'file-loader',
+                    options:{
+                        name:'../images/'
+                    }
+                }
+            ]
+        }
         ]
     }
 }
