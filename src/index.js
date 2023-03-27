ffc316a4bed6d96fe8b0f0e548c7793fc92036e1
Rodrigo Paiva <rodrigonpaiva@hotmail.com>
2023-03-27 21:48:57 +0200

    Create menu,slideshow, boxServices, logosSponsors and galleryPictures

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import './sass/style.scss';
+import * as bootstrap from 'bootstrap';
+
+const offcanvasElementList = document.querySelectorAll('.offcanvas')
+const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
 
